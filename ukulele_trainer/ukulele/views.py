ae9c4656232e9d37069bcabb5e9f31c3d1d2a9d3
MarekPisarcik <pisar385@gmail.com>
2025-08-31 15:43:28 +0200

    Pripojenie CSS
    Pripojenie CSS a funkčný server

--- ukulele_trainer/ukulele/views.py
+++ ukulele_trainer/ukulele/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse, HttpResponseNotFound
+from django.template import loader
 # Create your views here.
+
+def home(request,):
+    return render(request, 'ukulele/index.html')
--- ukulele_trainer/ukulele_trainer/urls.py
+++ ukulele_trainer/ukulele_trainer/urls.py
@@ -1,7 +1,8 @@
-
+# Konfiguračná zložka urls.py
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('ukulele.urls')),
 ]
