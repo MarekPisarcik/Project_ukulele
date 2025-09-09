f7b6101a524ce418a8d5647371be8902b3cd50de
MarekPisarcik <pisar385@gmail.com>
2025-09-09 16:12:58 +0200

    Registrácia a navigácia
    Pridaná navigácia, prihlasovanie, odhlasovanie a registrácia.

--- ukulele_trainer/ukulele/urls.py
+++ ukulele_trainer/ukulele/urls.py
@@ -1,10 +1,14 @@
 # URLS v aplikacii ukulele
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
      path('', views.home, name='home'),
-     path('trainer/', views.trainer, name='trainer'),  
+     path('trainer/', views.trainer, name='trainer'),
+     path('login/', auth_views.LoginView.as_view(template_name='ukulele/login.html'), name='login'),
+     path('logout/', auth_views.LogoutView.as_view(template_name='ukulele/logged_out.html'), name='logout'),
+     path('register/', views.register, name='register'),
 ]
 
 
--- ukulele_trainer/ukulele/views.py
+++ ukulele_trainer/ukulele/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound
 from django.template import loader
+from django.contrib.auth import login
+from .forms import CustomUserCreationForm
 # Create your views here.
 
 def home(request,):
@@ -8,3 +10,15 @@ def home(request,):
 
 def trainer(request,):
     return render(request, 'ukulele/trainer.html')
+
+def register(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = CustomUserCreationForm()
+    
+    return render(request, "ukulele/register.html", {"form": form})
